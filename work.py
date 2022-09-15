8595966b0e5cb15173664b3d0162ec393e8e60a3
tianranling <113494685+tianranling@users.noreply.github.com>
2022-09-15 17:48:56 +0800

    Add files via upload
    学号：2020144140   姓名：郑宇   第一次作业

---
+++ work.py
@@ -0,0 +1,18 @@
+from turtle import*
+i=1
+j=1
+pendown()
+forward(100)
+while i<6:
+      right(60)
+      forward(100)
+      i=i+1
+forward(100)
+right(120)
+forward(100)
+while j<6:
+      left(60)
+      forward(100)
+      right(120)
+      forward(100)
+      j=j+1
